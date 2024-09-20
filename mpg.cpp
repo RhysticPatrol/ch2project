d03f09fcf959c17e158a862ac8947000b5f02005
RhysticPatrol <stevenswilson7798@gmail.com>
2024-09-19 20:34:41 -0400

    Update mpg.cpp

--- mpg.cpp
+++ mpg.cpp
@@ -5,13 +5,13 @@ using namespace std;
 int main()
 {
     int gasTank = 20;
-    int mpgTown = 23;
-    float mpgCity = 28.9;
-    double distanceTown = (gasTank * mpgTown);
-    double distanceCity = (gasTank * mpgCity);
+    float mpgTown = 23.5;
+    float mpgHighway = 28.9;
+    double distanceTown = (gasTank * mpgTown); //first equation to calculate miles per gallon when driving in town
+    double distanceHighway = (gasTank * mpgHighway); //second equation to calulate miles per gallon when driving in the highway
 
-    cout << "Travel Distance in Town: " << distanceTown << "\n";
-    cout << "Travel Distance on the Highway: " << distanceCity << "\n";
+    cout << "Travel Distance in Town: " << distanceTown << "\n"; //display the result of first equation
+    cout << "Travel Distance on the Highway: " << distanceHighway << "\n"; //display the result of second equation
 
     return 0;
 }
