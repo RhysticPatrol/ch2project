7feed8544ff91c7f805b161394eb5d878c83305a
RhysticPatrol <stevenswilson7798@gmail.com>
2024-09-19 20:30:51 -0400

    Update energydrink.cpp

--- energydrink.cpp
+++ energydrink.cpp
@@ -4,12 +4,13 @@
 using namespace std;
 int main()
 {
+    //establish data needed for equations
     int Customers = 16500;
     float energyDrink = 0.15;
     float preferCitris = 0.58;
 
-    cout << "Customers that drink more than one energy drink per week: " << (Customers * energyDrink) << "\n";
-    cout << "Customers that prefer citrus flavored energy drinks: " << (Customers * preferCitris) << "\n";
+    cout << "Customers that drink more than one energy drink per week: " << (Customers * energyDrink) << "\n"; //display the result of customers multiplied by the energy drink percentage
+    cout << "Customers that prefer citrus flavored energy drinks: " << (Customers * preferCitris) << "\n"; //display the result of customers multiplied by the percentage of people who prefer citrus flavored drinks
 
     return 0;
 }
