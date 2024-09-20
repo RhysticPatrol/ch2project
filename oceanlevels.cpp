8215c045d463801d01c30a7e501cb68d3e124100
RhysticPatrol <stevenswilson7798@gmail.com>
2024-09-19 20:37:20 -0400

    Update oceanlevels.cpp

--- oceanlevels.cpp
+++ oceanlevels.cpp
@@ -3,12 +3,12 @@
 
 using namespace std;
 int main() {
-    float oceanRise = 1.5;
-    float fiveYears = 5 * oceanRise;
-    float sevenYears = 7 * oceanRise;
-    float tenYears = 10 * oceanRise;
+    float oceanRise = 1.5; //millimeters that ocean rises a year
+    float fiveYears = 5 * oceanRise; // first equation to calculate the rise after 5 years
+    float sevenYears = 7 * oceanRise; // equation to calculate the rise after 7 years 
+    float tenYears = 10 * oceanRise; // equation to calculate the rise after 10 years
 
-    cout << fiveYears << "\n" ;
+    cout << fiveYears << "\n" ; //displaying results for equations
     cout << sevenYears << "\n";
     cout << tenYears << "\n";
 
